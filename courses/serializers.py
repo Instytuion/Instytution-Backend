1cd45a868219774f8c29455a63c59d257dcb6009
mohammedanaz <mohammedanaz@gmail.com>
2024-09-17 22:13:16 +0530

    #43v2 course level field added to serializer. By Anaz

--- courses/serializers.py
+++ courses/serializers.py
@@ -13,7 +13,10 @@ class RetrieveCourseSerializer(serializers.ModelSerializer):
     week_descriptions = serializers.SerializerMethodField(read_only = True)
     class Meta:
         model    = Course
-        fields = ['name', 'price', 'program_name', 'duration', 'image', 'description', 'skill', 'prerequisite', 'week_descriptions']
+        fields = [
+            'name', 'price', 'program_name', 'duration', 'image', 
+            'description', 'skill', 'prerequisite', 'week_descriptions', 'course_level'
+            ]
 
     def get_program_name(self, obj):
         return self.context.get('program_name',None)
