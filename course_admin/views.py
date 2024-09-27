bd678176ae7e25d3666a3521ada438961c171994
aashiashfak <aashiashfak@gmail.com>
2024-09-27 21:09:43 +0530

    temp commit

--- course_admin/views.py
+++ course_admin/views.py
@@ -1,15 +1,16 @@
-from rest_framework import status
+from rest_framework import status ,generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
 from .serializers import CourseSerializer
 from accounts.permissions import IsCourseAdmin
 from courses.models import Course
 from urllib.parse import unquote
+from rest_framework.serializers import ValidationError
+from django.shortcuts import get_object_or_404
 
 class CourseCreateView(APIView):
     """ api for create new course and return its data """
-    permission_classes = [IsAuthenticated, IsCourseAdmin]
+    permission_classes = [IsCourseAdmin]
 
     def get(self, request, *args, **kwargs):
         course_name = unquote(kwargs.get('course_name', None))  
@@ -32,3 +33,23 @@ class CourseCreateView(APIView):
                 return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class CourseUpdateView(generics.UpdateAPIView):
+    """ API for updating an existing course """
+    permission_classes = [IsCourseAdmin]
+    serializer_class = CourseSerializer
+
+    def get_object(self):
+        """ Retrieve the course instance to be updated. """
+        course_name = self.kwargs.get('course_name', None)
+        if course_name:
+            return get_object_or_404(Course, name=unquote(course_name))
+        raise ValidationError({"course_name": "Course name is required."})
+
+    def patch(self, request, *args, **kwargs):
+        course = self.get_object()
+        serializer = self.get_serializer(course, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
