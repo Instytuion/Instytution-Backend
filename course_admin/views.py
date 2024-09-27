cbfb443a036d7b4e03aefb5c8fc9d266deb177a1
aashiashfak <aashiashfak@gmail.com>
2024-09-27 21:11:14 +0530

    #74 created update api for course model by aashi

--- course_admin/views.py
+++ course_admin/views.py
@@ -53,3 +53,4 @@ class CourseUpdateView(generics.UpdateAPIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
