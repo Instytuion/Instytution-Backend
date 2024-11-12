d70c4de814327d3149f5faf4c2a1c5c3a37e15a5
mohammedanaz <mohammedanaz@gmail.com>
2024-11-12 13:36:50 +0530

    #175v1 modified video bind logic to add redord id. by Anaz

--- class_room/serializers.py
+++ class_room/serializers.py
@@ -4,7 +4,7 @@ from .models import VideoChunks, SessionVideos
 class VideoChunkSerializer(serializers.ModelSerializer):
     class Meta:
         model = VideoChunks
-        fields = ['batch', 'video_chunk', 'chunk_serial']
+        fields = ['batch', 'video_chunk', 'chunk_serial', 'record_id']
 
 class SessionSerializer(serializers.ModelSerializer):
     video = serializers.FileField(use_url=True)
